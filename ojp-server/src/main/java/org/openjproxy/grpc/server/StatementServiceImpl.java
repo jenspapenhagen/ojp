24e701e05be4afe4ddc38455a1504fe3328940dd
Matheus Andre <matheusandr2@gmail.com>
2026-01-17 18:05:12 -0300

    Refactor xaRollback() to use Action pattern

--- ojp-server/src/main/java/org/openjproxy/grpc/server/StatementServiceImpl.java
+++ ojp-server/src/main/java/org/openjproxy/grpc/server/StatementServiceImpl.java
@@ -101,6 +101,7 @@ import org.openjproxy.grpc.server.action.session.TerminateSessionAction;
 import org.openjproxy.grpc.server.action.resource.CallResourceAction;
 import org.openjproxy.grpc.server.action.xa.XaPrepareAction;
 import org.openjproxy.grpc.server.action.xa.XaCommitAction;
+import org.openjproxy.grpc.server.action.xa.XaRollbackAction;
 
 @Slf4j
 public class StatementServiceImpl extends StatementServiceGrpc.StatementServiceImplBase {
@@ -1652,51 +1653,8 @@ public class StatementServiceImpl extends StatementServiceGrpc.StatementServiceI
     @Override
     public void xaRollback(com.openjproxy.grpc.XaRollbackRequest request,
             StreamObserver<com.openjproxy.grpc.XaResponse> responseObserver) {
-        log.debug("xaRollback: session={}, xid={}",
-                request.getSession().getSessionUUID(), request.getXid());
-
-        try {
-            Session session = sessionManager.getSession(request.getSession());
-            if (session == null || !session.isXA()) {
-                throw new SQLException("Session is not an XA session");
-            }
-
-            // Branch based on XA pooling configuration
-            if (xaPoolProvider != null) {
-                // **NEW PATH: Use XATransactionRegistry**
-                String connHash = session.getSessionInfo().getConnHash();
-                XATransactionRegistry registry = xaRegistries.get(connHash);
-                if (registry == null) {
-                    throw new SQLException("No XA registry found for connection hash: " + connHash);
-                }
-
-                XidKey xidKey = XidKey.from(convertXid(request.getXid()));
-                registry.xaRollback(xidKey);
-
-                // NOTE: Do NOT unbind XAConnection here - it stays bound for session lifetime
-                // XABackendSession will be returned to pool when OJP Session terminates
-            } else {
-                // **OLD PATH: Pass-through (legacy)**
-                if (session.getXaResource() == null) {
-                    throw new SQLException("Session does not have XAResource");
-                }
-                javax.transaction.xa.Xid xid = convertXid(request.getXid());
-                session.getXaResource().rollback(xid);
-            }
-
-            com.openjproxy.grpc.XaResponse response = com.openjproxy.grpc.XaResponse.newBuilder()
-                    .setSession(session.getSessionInfo())
-                    .setSuccess(true)
-                    .setMessage("XA rollback successful")
-                    .build();
-            responseObserver.onNext(response);
-            responseObserver.onCompleted();
-
-        } catch (Exception e) {
-            log.error("Error in xaRollback", e);
-            SQLException sqlException = (e instanceof SQLException) ? (SQLException) e : new SQLException(e);
-            sendSQLExceptionMetadata(sqlException, responseObserver);
-        }
+        XaRollbackAction.getInstance()
+                .execute(actionContext, request, responseObserver);
     }
 
     @Override
